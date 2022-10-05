708b0f5b5e8c48dad1ba3d919e989cbcf7a831bb
decipher3114 <shauryasiwach26@gmail.com>
2022-10-05 14:05:53 +0530

    fixed revive

--- revancify.sh
+++ revancify.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 revive(){
-    clear && echo "Script terminated" && rm -rf ./*cache ; tput cnorm ; cd ~ || : ; exit
+    clear && echo "Script terminated" && rm -rf /data/data/com.termux/files/home/storage/Revancify/*cache ; tput cnorm ; cd ~; exit
 }
 trap revive SIGINT
 
@@ -237,7 +237,6 @@ patchoptions()
     tmp=$(mktemp)
     dialog --begin 0 $leavecols --no-lines --infobox "█▀█ █▀▀ █░█ ▄▀█ █▄░█ █▀▀ █ █▀▀ █▄█\n█▀▄ ██▄ ▀▄▀ █▀█ █░▀█ █▄▄ █ █▀░ ░█░" 4 38 --and-widget --begin 5 0 --ascii-lines --title "Options File Editor" --editbox options.toml $fullpageheight $fullpagewidth 2> "$tmp" && mv "$tmp" ./options.toml
     tput civis
-    clear
     mainmenu
 }
 
