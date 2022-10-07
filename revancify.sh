dac18e695217188feaee8d1d1a3a122183cc5489
decipher3114 <shauryasiwach26@gmail.com>
2022-10-07 17:10:36 +0530

    added return to mainmenu

--- revancify.sh
+++ revancify.sh
@@ -13,7 +13,8 @@ internet()
     then
         return 0
     else
-        dialog --begin 0 $leavecols --keep-window --no-lines --no-shadow --infobox "█▀█ █▀▀ █░█ ▄▀█ █▄░█ █▀▀ █ █▀▀ █▄█\n█▀▄ ██▄ ▀▄▀ █▀█ █░▀█ █▄▄ █ █▀░ ░█░" 4 38 --and-widget --msgbox "Oops! No Internet Connection available.\n\nConnect to Internet and try again later" 5 25
+        dialog --begin 0 $leavecols --keep-window --no-lines --no-shadow --infobox "█▀█ █▀▀ █░█ ▄▀█ █▄░█ █▀▀ █ █▀▀ █▄█\n█▀▄ ██▄ ▀▄▀ █▀█ █░▀█ █▄▄ █ █▀░ ░█░" 4 38 --and-widget --msgbox "Oops! No Internet Connection available.\n\nConnect to Internet and try again later" 10 35
+        mainmenu
     fi
 }
 
