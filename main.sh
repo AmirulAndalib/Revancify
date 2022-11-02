c08fecf1ca6fe8f157434bbd49ec1095b0a3f36e
decipher3114 <shauryasiwach26@gmail.com>
2022-11-02 14:28:36 +0530

    remove ./

--- main.sh
+++ main.sh
@@ -299,14 +299,14 @@ app_dl()
         echo "$appname-$appver.apk already exists."
         echo ""
         sleep 0.5s
-        wget -q -c "$applink" -O ./"$appname"-"$appver".apk --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+        wget -q -c "$applink" -O "$appname"-"$appver".apk --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
         sleep 0.5s
     else
         rm -rf ./"$appname"* > /dev/null 2>&1
         echo " "
         echo "Downloading $appname-$appver.apk..."
         echo " "
-        wget -q -c "$applink" -O ./"$appname"-"$appver".apk --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+        wget -q -c "$applink" -O "$appname"-"$appver".apk --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
         sleep 0.5s
     fi
 }
