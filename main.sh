7247a10e634fc6cdd8c7f3641da88fd07da440f2
decipher3114 <shauryasiwach26@gmail.com>
2022-11-03 22:18:38 +0530

    remove dialog config

--- main.sh
+++ main.sh
@@ -329,7 +329,7 @@ dlmicrog()
     if "${header[@]}" --begin 4 0 --title '| MicroG Prompt |' --no-items --defaultno --keep-window --no-shadow --yesno "Vanced MicroG is used to run MicroG services without root.\nYouTube and YTMusic won't work without it.\nIf you already have MicroG, You don't need to download it.\n\n\n\n\n\nDo you want to download Vanced MicroG app?" "$fullpageheight" -1
     then
         internet
-        wget -q -c "https://github.com/inotia00/VancedMicroG/releases/download/v0.2.25.224113-224113002/microg.apk" -O "VancedMicroG-0.2.25.apk" --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"  2>&1 | stdbuf -o0 cut -b 64-65 | "${header[@]}" --title '| Downloader |' --gauge "App     : Vanced MicroG\nVersion : 0.2.25\nSize    : 10.5M" 10 30
+        wget -q -c "https://github.com/inotia00/VancedMicroG/releases/download/v0.2.25.224113-224113002/microg.apk" -O "VancedMicroG-0.2.25.apk" --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"  2>&1 | stdbuf -o0 cut -b 64-65 | "${header[@]}" --title '| Downloader |' --gauge "App     : Vanced MicroG\nVersion : 0.2.25\nSize    : 10.5M" 10 30 && tput civis
         [[ -f Vanced-MicroG.apk ]] && mv Vanced-MicroG.apk /storage/emulated/0/Revancify/ && termux-open /storage/emulated/0/Revancify/Vanced-MicroG.apk
     fi
     mainmenu
